2999bfce8425b4974ead7e6a41ea9c0ebc39abaf
h3nr1d3v <akame8416@gmail.com>
2024-11-08 17:34:12 -0500

    subiendo los archivos algo avanzado

---
+++ tailwind.config.mjs
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        light: {
+            primary: '#059669',
+            secondary: '#ecfdf5',
+            tertiary: '#34d399',
+            accent: '#022c22',
+        },
+        dark: {
+            primary: '#22d3ee',
+            secondary: '#083344',
+            tertiary: '#0891b2',
+            accent: '#cffafe'
+        }
+    },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Roboto Slab', 'serif'],
+      },
+      animation: {
+        'fade-in': 'fadeIn 0.5s ease-in-out',
+        'slide-up': 'slideUp 0.5s ease-out',
+        'slide-in': 'slideIn 0.5s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        slideUp: {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        slideIn: {
+          '0%': { transform: 'translateX(-20px)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
