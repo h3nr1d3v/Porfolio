02c720f27737588ba3deff763d9f1db9ba26aca3
h3nr1d3v <akame8416@gmail.com>
2024-11-10 12:13:49 -0500

    V0.1.1

---
+++ src/i18n/index.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslations from './en.json';
+import esTranslations from './es.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslations },
+      es: { translation: esTranslations },
+    },
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
